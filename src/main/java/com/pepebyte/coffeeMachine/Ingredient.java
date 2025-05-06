173e78f2fb7fb03ddce1071645133a080c7595e1
pepebyte <tishembitov@yandex.ru>
2025-05-06 16:26:35 +0500

    Add and update some functions

---
+++ src/main/java/com/pepebyte/coffeeMachine/Ingredient.java
@@ -0,0 +1,35 @@
+package com.pepebyte.coffeeMachine;
+
+public enum Ingredient {
+    WATER(
+            "Write how many ml of water do you want: ",
+            "Sorry, not enough water!",
+            "%d of water"
+    ),
+    MILK(
+            "Write how many ml of milk do you want: ",
+            "Sorry, not enough milk!",
+            "%d of milk"
+    ),
+    BEANS(
+            "Write how many grams of coffee beans do you want: ",
+            "Sorry, not enough coffee beans!",
+            "%d of coffee beans"
+    ),
+    CUPS(
+            "Write how many disposable cups of coffee do you want: ",
+            "Sorry, not enough cups!",
+            "%d of disposable cups"
+    );
+
+    public final String addMsg;
+    public final String emptyMsg;
+    public final String format;
+
+    Ingredient(String addMsg, String emptyMsg, String format) {
+        this.addMsg = addMsg;
+        this.emptyMsg = emptyMsg;
+        this.format = format;
+    };
+
+}
